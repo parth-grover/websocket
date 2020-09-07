bd32589ac9d7e2c0f1b387ef6a429d356cd5af7a
Parth Grover <parthgrover1994@gmail.com>
2020-09-07 09:19:13 +0530

    websocket

--- server.js
+++ server.js
@@ -1,32 +1,58 @@
-const express = require('express')
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
 
-const app = express()
+const app = express();
 
-const http = require('http')
+const http = require('http');
+const { stringify } = require('querystring');
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001;
 
+server.listen(port, () => {
+  console.log(`Listen on port ${port}`);
+});
 
+app.use(express.static(__dirname + '/public'));
 
-server.listen(port,()=>{
-	console.log(`Listen on port ${port}`)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+/*mongoose.connect(process.env.DATABASE_LOCAL,{
+	useNewUrlParser:true,
+	useCreateIndex:true,
+	useUnifiedTopology:true,
+	useFindAndModify:false
 })
+.then(() => console.log("Db connected successfully"));
 
-app.use(express.static(__dirname +'/public'));
+const webChatSchema = new mongoose.Schema({
+	name:String,
+	message:String
+});
+const Webchat = mongoose.model('Webchat',webChatSchema);
+const testwebchat = new Webchat({
+	name:"Aman",
+	message:"hello"
+});
+testwebchat.save().then(doc =>{
+	console.log(doc)
+}).catch(err =>{
+	console.log(err)
+})*/
 
-app.get('/',(req,res) =>{
-	res.sendFile(__dirname + '/index.html')
-})
+const socket_con = require('socket.io')(server);
 
-
-const socket_con = require('socket.io')(server) 
-
-socket_con.on('connection',(socket) =>{
-	console.log('socket connected')
-	socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
-    })
-})
-
+socket_con.on('connection', (socket) => {
+  console.log('socket connected');
+  //socket.on('message', (msg) => {
+  //socket.broadcast.emit('message', msg);
+  //});
+  socket.on('vue_msg', (msg) => {
+    socket.emit('re_vue_msg', msg);
+  });
+});
